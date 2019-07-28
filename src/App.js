e3166ecd79084cec0a9b763c011964ba3425a094
Moiz Ali <moiz1322@gmail.com>
2019-07-28 15:21:08 -0500

    Added Routing and withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Hats from './components/hats';
+
 import './App.css';
 import HomePage from './pages/homepage';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/hats" component={Hats}/>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/hats.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Hats = () => {
+    return <div>Hats</div>;
+};
+
+export default Hats;
