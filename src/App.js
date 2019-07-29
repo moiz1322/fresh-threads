066baca2b001fa0dfdfc46cd1fd47dfe71212a3b
Moiz Ali <moiz1322@gmail.com>
2019-07-29 12:40:13 -0500

    created header component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import ShopPage from './pages/shop/shop';
+import {Switch, Route} from 'react-router-dom';
 
 import './App.css';
+
+import ShopPage from './pages/shop/shop';
 import HomePage from './pages/homepage/homepage';
+import Header from './components/header/header.jsx'
 
 function App() {
   return (
-    <Router>
-      <div>
+    <div>
+      <Header />
+      <Switch>
         <Route exact path="/" component={HomePage}/>
-        <Route exact path="/shop" component={ShopPage}/>
-      </div>
-    </Router>
+        <Route path="/shop" component={ShopPage}/>
+      </Switch>
+    </div>
   );
 }
 
