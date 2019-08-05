91cab1148bb210a928e0f5efa2f39e18bb8abb66
Moiz Ali <moiz1322@gmail.com>
2019-08-05 13:37:31 -0500

    added selectors and imported into card-dropdown and card-icon

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -3,13 +3,14 @@ import { connect } from "react-redux";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { toggleShowCartDropdown } from "../../redux/cart/cart.actions.js";
+import { selectCartItemsCount } from "../../redux/cart/cart.selector.js";
 
 import "./cart-icon.scss";
 
-const CartIcon = ({ toggleShowCartDropdown }) => (
+const CartIcon = ({ toggleShowCartDropdown, itemCount }) => (
 	<div className="cart-icon" onClick={toggleShowCartDropdown}>
 		<ShoppingIcon className="shopping-icon" />
-		<span className="item-count">0</span>
+		<span className="item-count">{itemCount}</span>
 	</div>
 );
 
@@ -17,7 +18,11 @@ const mapDispatchToProps = dispatch => ({
 	toggleShowCartDropdown: () => dispatch(toggleShowCartDropdown())
 });
 
+const mapStateToProps = state => ({
+	itemCount: selectCartItemsCount(state)
+});
+
 export default connect(
-	null,
+	mapStateToProps,
 	mapDispatchToProps
 )(CartIcon);
